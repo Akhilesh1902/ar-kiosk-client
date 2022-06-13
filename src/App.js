1d4912602761c5727d0c0d3b7eeb48dc6f595f70
Akhilesh1902 <akhil1922002@gmail.com>
2022-06-13 22:12:54 +0530

    fixed the video not working issue

--- src/App.js
+++ src/App.js
@@ -5,32 +5,47 @@ import UI from './components/UI';
 import { io } from 'socket.io-client';
 
 function App() {
-  // const SERVER_URL = 'http://localhost:3030/';
-  const SERVER_URL = 'https://ar-kiosk-proto.herokuapp.com/';
+  const SERVER_URL = 'http://localhost:3030/';
+  // const SERVER_URL = 'https://ar-kiosk-proto.herokuapp.com/';
 
   const [socket, setSocket] = useState();
-
-  // let socket;
-  useEffect(() => {
-    if (!socket) console.log('no socket');
-
-    const c = async () => {
-      const s = io.connect(SERVER_URL);
-      setSocket(s);
-    };
-
-    c();
-    // if (!socket) {
-    //   socket =  io.connect(SERVER_URL);
-    //   console.log('connecting to socket');
-    // }
-    // eslint-disable-next-line
-  }, []);
 
   const userVid = useRef();
 
   const [image, setImage] = useState(null);
   const [vid, setVid] = useState(false);
+
+  // let socket;
+  useEffect(() => {
+    if (!socket) console.log('no socket');
+
+    const c = () => {
+      const s = io.connect(SERVER_URL);
+      setSocket(s);
+    };
+
+    c();
+
+    // eslint-disable-next-line
+  }, []);
+
+  useEffect(() => {
+    if (!vid) {
+      if (userVid.current?.srcObject) {
+        userVid.current.srcObject = null;
+      }
+      return;
+    }
+
+    const getVideo = async () => {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: true,
+      });
+      userVid.current.srcObject = stream;
+    };
+    getVideo();
+  }, [vid]);
 
   if (!socket) return null;
 
